19578534a332dd3f49ae5f0737c415e9e2fda531
Rachana Aithal <rachanaaithal88@gmail.com>
2023-11-25 18:56:39 -0500

    small change

--- NATS-Bench/Mobile/gather_mobile_data.py
+++ NATS-Bench/Mobile/gather_mobile_data.py
@@ -9,18 +9,18 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from generate_tflite_models import write_models
 
 
-def main(batch_size, dataset, loops, inf_temp_file, perf_temp_file):
+def main(batch_size, dataset, loops, inf_temp_file, perf_temp_file, idx):
     api = create('../NATS-tss-v1_0-3ffb9-simple', 'tss', fast_mode=True, verbose=False)
     models_folder = "models"
     results_folder = "results"
     performance_script = "do_perf.sh"
-    idx = 0
+    idx = idx
 
     while True:
         more_models = write_models(api, idx, batch_size, dataset, models_folder)
 
         # Call do_perf.sh shell script
-        subprocess.run(["sh", performance_script, models_folder, results_folder, "inf_temp.csv", "perf_temp.csv"])
+        subprocess.run(["sh", performance_script, models_folder, results_folder, inf_temp_file, perf_temp_file])
 
         # Remove all files in folder except index-history.txt
         for file in os.listdir(models_folder):
@@ -43,9 +43,10 @@ if __name__ == "__main__":
     parser.add_argument("--loops", type=int, help="Number of loops to run (default: run until all models are processed)")
     parser.add_argument("--inf_temp_file", type=str, default="inf_temp.csv", help="Inference temp file (default: 'inf_temp.csv')")
     parser.add_argument("--perf_temp_file", type=str, default="perf_temp.csv", help="Performance temp file (default: 'perf_temp.csv')")
+    parser.add_argument("--idx", type=int, default=0, help="Starting index for model processing")
 
     # Parse the arguments
     args = parser.parse_args()
 
     # Call main function with parsed arguments
-    main(args.batch_size, args.dataset, args.loops, args.inf_temp_file, args.perf_temp_file)
+    main(args.batch_size, args.dataset, args.loops, args.inf_temp_file, args.perf_temp_file, args.idx)
